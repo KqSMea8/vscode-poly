4f900cf594375a109c64b34998cace5e51f15012
hellogithub2014 <1197128237@qq.com>
2019-04-14 08:54:09 +0800

    素材

---
+++ 素材/Untitled-1.vue
@@ -0,0 +1,14 @@
+<template>
+  <span>{{ $t('campaign_buyingType') }}</span>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      key: 'campaign_choose',
+    }
+  },
+}
+</script>
+
